73d83b565f73b46c702d1dd791b0a8ee1001d134
Fuzz Pan <fuzz475366898@outlook.com>
2019-05-31 19:35:31 +0800

    V0.0.5

--- example/mnist_cls.py
+++ example/mnist_cls.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
+import torch
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 import sys
 sys.path.append('..')
 from model.dbn import DBN
 from model.cnn import CNN
-import torch
-from pandas import DataFrame
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def run(name, para):
     model = eval(name.upper()+'(**para)')
@@ -31,20 +30,13 @@ def dbn():
 def cnn():
     # CNN
     ''' 
-        head = ['conv_para', 'bn_type', 'pool_type', 'pool_para']
         conv_para: (in_channels(auto), out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True)
-        bn_type: 0, 1
-        pool_type: 0, 'Max', 'Avg', 'FractionalMax', 'AdaptiveMax', 'AdaptiveAvg'
+        pool_type: 'Max', 'Avg', 'FractionalMax', 'AdaptiveMax', 'AdaptiveAvg'
         pool_para: (kernel_size, stride=None, padding=0, dilation=1, return_indices=False, ceil_mode=False)
     ''' 
-    conv = DataFrame(
-            columns = ['conv_para', 'bn_type', 'pool_type', 'pool_para']
-            )
-    conv.loc[0] = [[3, 8],     1, '', [2]]
-    conv.loc[1] = [[6, (6,6)], 1, '',  0 ]
-    
+
     para = {'img_size': [1,28,28],
-            'conv_struct': conv,
+            'conv_struct': [[3, 8], ['M', 2], [6, (6,6)]],
             'conv_func': 'ReLU',
             'conv_dropout': 0.0,
             'struct': [150, 10],
