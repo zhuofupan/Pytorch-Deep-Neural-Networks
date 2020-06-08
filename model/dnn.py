d7916ed1f7c3d27ea22c81c2f2d23642d957cd3e
Fuzz Pan <fuzz475366898@outlook.com>
2020-06-08 09:04:56 +0800

    2020.06.08

--- model/dnn.py
+++ model/dnn.py
@@ -30,20 +30,3 @@ class DNN(Module):
         x = self._feature(x)
         x = self._output(x)
         return x
-
-if __name__ == '__main__':
-    
-    parameter = {'struct': [784,400,100,10],
-                 'hidden_func': ['g', 'a'],
-                 'output_func': 'x',
-                 'dropout': 0.0,
-                 'task': 'cls',
-                 'flatten': True}
-    
-    model = DNN(**parameter)
-    
-    model.load_mnist('../data', 128)
-    
-    for epoch in range(1, 3 + 1):
-        model.batch_training(epoch)
-        model.test(epoch)
